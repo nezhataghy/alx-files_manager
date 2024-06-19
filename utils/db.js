168caa0042fcb1179346a024cf00825aa94086d3
nezhataghy <nezhataghy@gmail.com>
2024-06-19 21:13:28 +0000

    MongoDB utils

--- utils/db.js
+++ utils/db.js
@@ -1,1 +1,40 @@
-#!/usr/bin/node
+import mongodb from 'mongodb';
+import Collection from 'mongodb/lib/collection';
+import envLoader from './env_loader';
+
+class DBClient {
+  constructor() {
+    envLoader();
+    const port = process.env.DB_PORT || 27017;
+    const host = process.env.DB_HOST || 'localhost';
+    const db = process.env.DB_DATABASE || 'files_manager';
+    const URL = `mongodb://${host}:${port}/${db}`;
+
+    this.client = new mongodb.MongoClient(URL);
+    this.client.connect();
+  }
+
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+
+  async usersCollection() {
+    return this.client.db().collection('users');
+  }
+
+  async filesCollection() {
+    return this.client.db().collection('files');
+  }
+}
+
+export const dbClient = new DBClient();
+export default dbClient;
