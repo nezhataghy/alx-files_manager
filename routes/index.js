732a6f7e9f4e4360c7d2595945dabfe431fb91d0
nezhataghy <nezhataghy@gmail.com>
2024-06-19 23:07:23 +0000

    API

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ import { Express } from 'express';
 import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
 import UsersController from '../controllers/UsersController';
-import FilesController from '../controllers/FilesController';
 import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
 import { APIError, errorResponse } from '../middlewares/error';
 
