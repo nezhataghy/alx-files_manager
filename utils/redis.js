0c4d1f9f77831b6378116058e784a504b92283bd
nezhataghy <nezhataghy@gmail.com>
2024-06-19 19:54:26 +0000

    Redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,1 +1,36 @@
-#!/usr/bin/node
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.isClientConnected = true;
+    this.client.on('error', (err) => {
+      console.error('Redis client failed to connect:', err.message || err.toString());
+      this.isClientConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
+    });
+  }
+
+  isAlive() {
+    return this.isClientConnected;
+  }
+
+  async get(key) {
+    return promisify(this.client.get).bind(this.client)(key);
+  }
+
+  async set(key, value, duration) {
+    await promisify(this.client.set)
+      .bind(this.client)(key, duration, value);
+  }
+
+  async del(key) {
+    await promisify(this.client.del).bind(this.client)(key);
+  }
+}
+
+export const redisClient = new RedisClient();
+export default redisClient;
