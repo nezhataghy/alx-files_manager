58f63ace551d637c10ded7f851b070083d611a5d
nezhataghy <nezhataghy@gmail.com>
2024-06-19 22:15:29 +0000

    First API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,1 +1,22 @@
-#!/usr/bin/node
+/* eslint-disable import/no-named-as-default */
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class AppController {
+  static getStatus(req, res) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    res.send(status);
+  }
+
+  static getStats(req, res) {
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+      .then(([usersCount, filesCount]) => {
+        res.status(200).json({ users: usersCount, files: filesCount });
+      });
+  }
+}
+
+export default AppController;
