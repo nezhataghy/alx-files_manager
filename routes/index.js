c8f3c6cbfd3daf6be6e699bd9790be5aa1908efd
nezhataghy <nezhataghy@gmail.com>
2024-06-20 00:52:28 +0000

    filescontroller

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import { Express } from 'express';
 import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
 import UsersController from '../controllers/UsersController';
+import FilesController from '../controllers/FilesController';
 import { basicAuthenticate, xTokenAuthenticate } from '../auth';
 import { APIError, errorResponse } from '../error';
 
